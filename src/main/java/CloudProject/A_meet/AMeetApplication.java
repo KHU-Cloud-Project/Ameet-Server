6efea490a82eb75cf3955ecb25d10a75a654c0bd
SuminSSon <itoodo12@gmail.com>
2024-10-24 21:33:59 +0900

    Initialize project

---
+++ src/main/java/CloudProject/A_meet/AMeetApplication.java
@@ -0,0 +1,13 @@
+package CloudProject.A_meet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AMeetApplication.class, args);
+	}
+
+}
