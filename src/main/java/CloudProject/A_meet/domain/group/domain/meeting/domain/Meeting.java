dfac749f6f4d2c5d4eb14e01ba6b53467a33e920
SuminSSon <itoodo12@gmail.com>
2024-10-29 19:32:03 +0900

    refactor: rename group -> team

--- src/main/java/CloudProject/A_meet/domain/group/domain/meeting/domain/Meeting.java
+++ src/main/java/CloudProject/A_meet/domain/group/domain/meeting/domain/Meeting.java
@@ -1,8 +1,9 @@
 package CloudProject.A_meet.domain.group.domain.meeting.domain;
 
-import CloudProject.A_meet.domain.group.domain.group.domain.Team;
+import CloudProject.A_meet.domain.group.domain.team.domain.Team;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -32,4 +33,6 @@ public class Meeting {
 
     @Column(nullable = false)
     private String title;
+
+    private Long duration;
 }
--- src/main/java/CloudProject/A_meet/domain/group/domain/team/domain/UserTeam.java
+++ src/main/java/CloudProject/A_meet/domain/group/domain/team/domain/UserTeam.java
@@ -1,34 +1,31 @@
-package CloudProject.A_meet.domain.group.domain.group.domain;
+package CloudProject.A_meet.domain.group.domain.team.domain;
 
+import CloudProject.A_meet.domain.group.domain.user.domain.User;
 import CloudProject.A_meet.global.common.model.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.springframework.cglib.core.Local;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
-
-@Table(name="teams")
+@Table(name="user_team")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @ToString
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-public class Team extends BaseTimeEntity {
+public class UserTeam extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
-    private Long teamId;
+    private Long userTeamId;
 
-    @Column(nullable = false)
-    private String name;
+    @ManyToOne
+    @JoinColumn(name="userId")
+    private User userId;
 
-    private String description;
-
-    @Column(nullable = false)
-    private long maxPeople;
+    @ManyToOne
+    @JoinColumn(name="team_id")
+    private Team teamId;
 }
