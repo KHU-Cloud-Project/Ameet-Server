90172b62cb1d38b7a68cb7cedcbae4bdb3d79994
Sujin Kim <su10jin11@khu.ac.kr>
2024-11-01 19:35:20 +0900

    feat : teamrepository 추가

---
+++ src/main/java/CloudProject/A_meet/domain/group/domain/team/repository/TeamRepository.java
@@ -0,0 +1,12 @@
+package CloudProject.A_meet.domain.group.domain.team.repository;
+
+import CloudProject.A_meet.domain.group.domain.team.domain.Team;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    Optional<Team> findByTeamId(Long teamId);
+}
