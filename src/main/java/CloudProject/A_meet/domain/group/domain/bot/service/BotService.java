8260fe83a708aabc1a9e21b8c2381ebf772942f8
Sujin Kim <su10jin11@khu.ac.kr>
2024-11-07 04:45:23 +0900

    feat : 요약봇 호출 API 에러 설정

--- src/main/java/CloudProject/A_meet/domain/group/domain/bot/service/BotService.java
+++ src/main/java/CloudProject/A_meet/domain/group/domain/bot/service/BotService.java
@@ -6,8 +6,8 @@ import CloudProject.A_meet.domain.group.domain.bot.dto.response.SummaryBotRespon
 import CloudProject.A_meet.domain.group.domain.bot.repository.BotRepository;
 import CloudProject.A_meet.domain.group.domain.meeting.domain.Meeting;
 import CloudProject.A_meet.domain.group.domain.meeting.repository.MeetingRepository;
-import CloudProject.A_meet.domain.group.domain.note.domain.Note;
-import CloudProject.A_meet.domain.group.domain.note.repository.NoteRepository;
+import CloudProject.A_meet.global.common.error.exception.CustomException;
+import CloudProject.A_meet.global.common.error.exception.ErrorCode;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,27 +18,18 @@ import org.springframework.stereotype.Service;
 public class BotService {
     private final BotRepository botRepository;
     private final MeetingRepository meetingRepository;
-    private final NoteRepository noteRepository;
 
     public SummaryBotResponse summaryBot(Long meetingId) {
         Meeting meeting = meetingRepository.findById(meetingId)
-                .orElseThrow(() -> new IllegalArgumentException("Meeting not found with ID: " + meetingId));
-
-        Note note = noteRepository.findByMeetingId(meetingId)
-                .orElseThrow(() -> new IllegalArgumentException("Note not found with Meeting ID: " + meetingId));
-
+                .orElseThrow(() -> new CustomException(ErrorCode.MEETING_NOT_FOUND));
 
         Bot bot = Bot.builder()
                 .meetingId(meeting)
-                .noteId(note)
                 .type(BotType.SUMMARY)
                 .content("Summary content")
                 .build();
 
         Bot savedBot = botRepository.save(bot);
-
         return new SummaryBotResponse(meetingId, savedBot.getBotId(), savedBot.getContent());
     }
-
-
 }
