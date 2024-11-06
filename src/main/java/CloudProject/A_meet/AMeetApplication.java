52e240b51a37a823f17d66eece8bcf7decef5651
Sungah <sungah0815@gmail.com>
2024-11-06 21:59:18 +0900

    FEAT: @EnableJpaAuditing 어노테이션 추가

--- src/main/java/CloudProject/A_meet/AMeetApplication.java
+++ src/main/java/CloudProject/A_meet/AMeetApplication.java
@@ -2,7 +2,9 @@ package CloudProject.A_meet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class AMeetApplication {
 
