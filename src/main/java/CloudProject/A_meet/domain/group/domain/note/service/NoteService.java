f53c1321399f66baa82f8279a28fa7f305ef3767
Sungah <sungah0815@gmail.com>
2024-11-14 17:17:24 +0900

    FEAT: 회의록 서비스 구조

--- src/main/java/CloudProject/A_meet/domain/group/domain/note/service/NoteService.java
+++ src/main/java/CloudProject/A_meet/domain/group/domain/note/service/NoteService.java
@@ -1,4 +1,4 @@
 package CloudProject.A_meet.domain.group.domain.note.service;
 
-public class NoteService {
+public interface NoteService {
 }
---
+++ src/main/java/CloudProject/A_meet/domain/group/domain/note/service/impl/NoteServiceImpl.java
@@ -0,0 +1,10 @@
+package CloudProject.A_meet.domain.group.domain.note.service.impl;
+
+import CloudProject.A_meet.domain.group.domain.note.service.NoteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NoteServiceImpl implements NoteService {
+}
