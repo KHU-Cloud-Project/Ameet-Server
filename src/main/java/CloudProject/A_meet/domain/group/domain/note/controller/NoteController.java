71a29201ab9eec977855c7ed7fd57addef58ec70
Sungah <sungah0815@gmail.com>
2024-11-14 17:18:08 +0900

    FEAT: 회의록 컨트롤러 어노테이션 추가

--- src/main/java/CloudProject/A_meet/domain/group/domain/note/controller/NoteController.java
+++ src/main/java/CloudProject/A_meet/domain/group/domain/note/controller/NoteController.java
@@ -1,4 +1,21 @@
 package CloudProject.A_meet.domain.group.domain.note.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 회의록 관련 Controller
+ * - 회의록 수동 생성, 상세조회, 목록조회 요청
+ * - 회의록 검색 요청
+ *
+ * @author sungah
+ * */
+@RestController
+@RequestMapping("/api/v1/note")
+@Tag(name = "Note", description = "Note API")
+@RequiredArgsConstructor
 public class NoteController {
 }
