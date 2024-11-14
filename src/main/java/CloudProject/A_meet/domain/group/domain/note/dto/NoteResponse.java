1bc2b6c4ff4f0d5fc7a2f0120abba6ccbc9c5905
Sungah <sungah0815@gmail.com>
2024-11-14 18:49:21 +0900

    FEAT: 회의록 상세 조회 API 구현

---
+++ src/main/java/CloudProject/A_meet/domain/group/domain/note/dto/NoteResponse.java
@@ -0,0 +1,32 @@
+package CloudProject.A_meet.domain.group.domain.note.dto;
+
+import CloudProject.A_meet.domain.group.domain.note.domain.Note;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@Getter
+public class NoteResponse {
+
+    private Long meetingId;
+    private Long noteId;
+    private String title;
+    private String content;
+    private String fileUrl;
+    private LocalDateTime createdAt;
+
+    public static NoteResponse of(Note note) {
+        return NoteResponse.builder()
+                .meetingId(note.getMeetingId().getMeetingId())
+                .noteId(note.getNoteId())
+                .title(note.getTitle())
+                .content(note.getContent())
+                .fileUrl(note.getFileUrl())
+                .createdAt(note.getCreatedAt())
+                .build();
+    }
+}
--- src/main/java/CloudProject/A_meet/domain/group/domain/note/service/NoteService.java
+++ src/main/java/CloudProject/A_meet/domain/group/domain/note/service/NoteService.java
@@ -1,4 +1,9 @@
 package CloudProject.A_meet.domain.group.domain.note.service;
 
+import CloudProject.A_meet.domain.group.domain.note.dto.NoteResponse;
+
 public interface NoteService {
+
+    // 1. 회의록 상세 조회
+    NoteResponse getNoteDetail(Long meetingId);
 }
