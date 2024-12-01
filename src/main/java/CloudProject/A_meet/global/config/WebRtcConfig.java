899fead8cebc78969012da2b983e284ff715222d
SuminSSon <itoodo12@gmail.com>
2024-12-02 03:54:19 +0900

    refactor: change api URL

--- src/main/java/CloudProject/A_meet/global/config/WebRtcConfig.java
+++ src/main/java/CloudProject/A_meet/global/config/WebRtcConfig.java
@@ -16,7 +16,7 @@ public class WebRtcConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(signalingHandler, "/webrtc")
+        registry.addHandler(signalingHandler, "/api/v1/meeting/join")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
