4c967d50d1b1bf1e955c5a5188c57e948b07c581
Sujin Kim <108571492+cowboysj@users.noreply.github.com>
2024-12-01 19:54:31 +0900

    fix : webconfig 수정

--- src/main/java/CloudProject/A_meet/global/config/WebConfig.java
+++ src/main/java/CloudProject/A_meet/global/config/WebConfig.java
@@ -12,7 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:5173",
-                        "https://localhost:5173"
+                        "https://localhost:5173",
+                        "https://127.0.0.1:5173",
+                        "http://127.0.0.1:5173"
                 )
                 .allowedMethods("GET", "POST", "PATCH", "DELETE", "PUT")
                 .allowedHeaders("*")
