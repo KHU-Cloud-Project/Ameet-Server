8d463db1e003c48df2588c3dda59a221e84c3ba9
Sujin Kim <su10jin11@khu.ac.kr>
2024-12-10 17:21:55 +0900

    fix : cors 설정

--- src/main/java/CloudProject/A_meet/global/config/WebConfig.java
+++ src/main/java/CloudProject/A_meet/global/config/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://localhost:5173",
                         "https://localhost:5173",
                         "https://127.0.0.1:5173",
-                        "http://127.0.0.1:5173"
+                        "http://127.0.0.1:5173",
+                        "https://file-space.store"
                 )
                 .allowedMethods("GET", "POST", "PATCH", "DELETE", "PUT")
                 .allowedHeaders("*")
